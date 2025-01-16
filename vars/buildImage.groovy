5e6fca7660de1b47378d4be49d42675a1b7d43dc
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-01-16 12:24:16 -0800

    update build Image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
-def call(String imageName, ) {
+def call(String imageName) {
   echo "building the docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
       sh "docker build -t $imageName ."
