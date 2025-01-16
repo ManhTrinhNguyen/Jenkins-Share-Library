64b3258b441541e693fff011a489545c2a16d385
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-01-16 11:29:10 -0800

    Update build image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
   echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nguyenmanhtrinh/demo-app:jma-4.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nguyenmanhtrinh/demo-app:jma-4.0'
-    }
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      sh 'docker build -t nguyenmanhtrinh/demo-app:jma-4.0 .'
+      sh 'echo $PASS | docker login -u $USER --password-stdin'
+      sh 'docker push nguyenmanhtrinh/demo-app:jma-4.0'
+  }
 }
