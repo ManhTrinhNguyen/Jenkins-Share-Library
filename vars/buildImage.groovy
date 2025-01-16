5b5e2dfae8803f2afba28c82e15538e62946dead
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-01-16 11:24:23 -0800

    Update credentials

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
   echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nguyenmanhtrinh/demo-app:jma-4.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push nguyenmanhtrinh/demo-app:jma-4.0'
     }
 }
